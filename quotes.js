ae14082f36c1f5984807d87176e623ec4c7a8266
Praveen Vegesna <praveenvegesna@Praveens-MacBook-Pro.local>
2023-09-30 22:13:38 +0530

    Adds Quote Generator

---
+++ quotes.js
@@ -0,0 +1,132 @@
+const localQuotes = [
+    {
+        "text": "Today is the tomorrow we worried about yesterday.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "It's easier to see the mistakes on someone else's paper.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Every man dies. Not every man really lives.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "The day is already blessed, find peace within it.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "From small beginnings come great things.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "It takes both sunshine and rain to make a rainbow.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "A beautiful thing is never perfect.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Every new day is another chance to change your life.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Some pursue happiness, others create it.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "We do what we do because we believe.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "The harder you fall, the higher you bounce.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Everyone smiles in the same language.",
+        "author": "Anonymousk",
+        "tag": ""
+    },
+    {
+        "text": "You can do what's reasonable or you can decide what's possible.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Friendship isn't a big thing. It's a million little things.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "A good plan today is better than a perfect plan tomorrow.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Someone remembers, someone cares; your name is whispered in someone's prayers.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Don't be pushed by your problems; be led by your dreams.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Change your thoughts, change your life!",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Today is the tomorrow you worried about yesterday.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "The day always looks brighter from behind a smile.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "A stumble may prevent a fall.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Put your future in good hands your own.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "What you see depends on what you're looking for.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Worry gives a small thing a big shadow.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "To get something you never had, you have to do something you never did.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Be thankful when you don't know something for it gives you the opportunity to learn.",
+        "author": "Anonymous",
+        "tag": "general"
+    }
+]
